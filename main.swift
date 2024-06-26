692e698d544d6c3da60f57bb7250534295e90792
Armaan Khan <kh.armaan535@gmail.com>
2024-06-26 02:29:49 +0000

    I was making mistakes in making long comments

--- main.swift
+++ main.swift
@@ -16,14 +16,24 @@ print("floating number is \(flNum) and integer number is \(intNum)")
 
 //String Manipulation
 
-"""
+/*
 3. Create a string variable to store a sentence.
 Find the length of the string and print it.
 Convert the entire string to uppercase and print it.
-"""
+*/
 
 var sT = "Hello World"
 print("Length of the string is \(sT.count)")
 
 sT = sT.uppercased()
 print(sT)
+
+/* Character Operations:
+
+Create a variable to store a character.
+Print the Unicode value of the character.
+Check if the character is a letter or a number and print the result.
+*/
+
+var ch = "A"
+print("Unicode of \(ch) is  \(ch.unicodeScalars.first!.value)")
